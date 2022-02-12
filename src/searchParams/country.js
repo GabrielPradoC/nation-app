9d43ca27351709c05275c1039945be8091438cdd
Gabriel Prado <gabriel.prado@helpper.com.br>
2022-02-12 15:19:02 -0300

    fix: Change folder name bcz windows

--- src/searchParams/country.js
+++ src/searchParams/country.js
@@ -1,4 +1,4 @@
-import getOpts from "../aux/getOpts.js";
+import getOpts from "../auxFiles/getOpts.js";
 import {countriesObjArr} from "./countries.js";
 
 const countryOptElsArr = getOpts(countriesObjArr);
--- src/searchParams/language.js
+++ src/searchParams/language.js
@@ -1,4 +1,4 @@
-import getOpts from "../aux/getOpts.js";
+import getOpts from "../auxFiles/getOpts.js";
 import {l as langObjArr}  from"./languages.js";
 const Arr = getOpts(langObjArr);
 export default Arr;
